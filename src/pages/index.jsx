eb0dcc6fd7ef27a0313fd2829a945d415e21b5cb
andria-hibe <andriacohibe@gmail.com>
2020-07-18 23:36:42 +1200

    Create home page components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,77 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
 
-import { Background } from '../components/global'
+import { device, GlobalStyles } from '../components/globalStyle'
+
+const MainContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  @media ${device.tablet} {
+    height: 100vh;
+  }
+`
 
 const HomeHeader = styled.h1`
-  font-size: 3rem;
-  text-align: center;
+  font-size: 4rem;
   color: white;
+  margin: 0;
+  text-align: center;
 `
+
+const HomeSubtitle = styled.h2`
+  font-size: 2rem;
+  color: #5B5B5B;
+  margin: 0;
+`
+
+const Divider = styled.hr`
+  width: 20em;
+  border: 0.1em solid #828282;
+  @media ${device.laptop} {
+    width: 30em;
+  }
+`
+
+const CardContainer = styled.div`
+  @media ${device.laptop} {
+    display: flex;
+    flex-direction: row;
+  }
+`
+
+const HomeCard = styled.div`
+  background: linear-gradient(#FBC2EB, #A6C1EE);
+  padding: 50px;
+  box-shadow: -0.5em -0.5em 0.1em rgba(91, 91, 91, 0.25);
+  border-radius: 0.5em;
+  margin: 2.5em;
+  font-size: 2rem;
+`
+
+const linkStyle = { 
+  color: 'white', 
+  textDecoration: 'none' 
+}
 
 export default function Home() {
   return (
-    <Background gradient>
-      <HomeHeader>Andria Hibe</HomeHeader>
-      <p>Heya</p>
-    </Background>
+    <>
+      <GlobalStyles gradient />
+      <MainContainer>
+        <HomeHeader>Andria Hibe</HomeHeader>
+        <Divider />
+        <HomeSubtitle>Full Stack Developer</HomeSubtitle>
+        <CardContainer>
+          <HomeCard><Link to='/resume' style={linkStyle}>Resumé</Link></HomeCard>
+          <HomeCard><Link to='/projects' style={linkStyle}>Projects</Link></HomeCard>
+          <HomeCard><Link to='/about' style={linkStyle}>About</Link></HomeCard>
+        </CardContainer>
+      </MainContainer>
+    </>
   )
 }
