efdf41881e87d7f933f9912486376592863b7167
andria-hibe <andriacohibe@gmail.com>
2020-08-23 22:31:52 +1200

    Change button style for hover

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { device, GlobalStyles } from '../components/globalStyle'
 
 const GradientButton = styled.button`
   background-image: linear-gradient(to right, #a6c1ee, #fbc2eb);
   border: none;
   text-align: center;
   border-radius: 0.5em;
-  height: 2.75em;
   color: white;
+  padding: 1em 0.75em;
 
   :hover {
     color: #555555;
+    cursor: pointer;
+  }
+
+  @media ${device.tablet} {
+    font-size: 1.25rem;
   }
 `
 
