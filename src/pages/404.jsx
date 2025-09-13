feacd72f53cee09cb6120045e71f68e2fd236c7e
Andria Hibe <andriacohibe@gmail.com>
2025-09-14 01:17:50 +1200

    Add interactive portfolio link with pixel rabbit icon
    - Add pixel rabbit icon to top-right corner of landing page
    - Include 'Check out my interactive portfolio!' text with Monogram font
    - Create clickable link to 2D portfolio cottage
    - Move monogram.ttf to src/fonts/ directory
    - Implement clean horizontal layout with proper spacing
    - Add hover effects for better user interaction

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,12 +7,24 @@ import SEO from '../components/seo'
 import { GlobalStyles } from '../components/globalStyle'
 import SplitLayout from '../split-layout/split-layout'
 
+const CenteredContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  min-height: 60vh;
+  gap: 1.5rem;
+`
+
 const CenteredH3 = styled.h3`
   text-align: center;
+  margin: 0;
 `
 
 const CenteredH4 = styled.h4`
   text-align: center;
+  margin: 0;
 `
 
 export default function About() {
@@ -29,19 +41,20 @@ export default function About() {
   return (
     <SplitLayout>
       <GlobalStyles />
-      <GatsbyImage
-        style={{
-          maxWidth: '250px',
-          maxHeight: '250px',
-          margin: '0 auto',
-        }}
-        image={getImage(data.robot)}
-        alt="Robot graphic"
-      />
-      <CenteredH3>Oh no... We cannot find that page :(</CenteredH3>
-      <CenteredH4>
-        Check out the navigation menu to get back on track.
-      </CenteredH4>
+      <CenteredContainer>
+        <GatsbyImage
+          style={{
+            maxWidth: '250px',
+            maxHeight: '250px',
+          }}
+          image={getImage(data.robot)}
+          alt="Robot graphic"
+        />
+        <CenteredH3>Oh no... We cannot find that page :(</CenteredH3>
+        <CenteredH4>
+          Check out the navigation menu to get back on track.
+        </CenteredH4>
+      </CenteredContainer>
     </SplitLayout>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Link as GatsbyLink } from 'gatsby'
-import styled from 'styled-components'
+import { Link as GatsbyLink, graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import styled, { createGlobalStyle } from 'styled-components'
 
 import SEO from '../components/seo'
 import { device, GlobalStyles } from '../components/globalStyle'
@@ -9,6 +10,16 @@ import {
   TiSocialAtCircular,
   TiSocialLinkedinCircular,
 } from 'react-icons/ti'
+
+const MonogramFont = createGlobalStyle`
+  @font-face {
+    font-family: 'Monogram';
+    src: url('../fonts/monogram.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+`
 
 const MainContainer = styled.div`
   height: 100vh;
@@ -83,9 +94,71 @@ const StyledEmailLinkIcon = styled(TiSocialAtCircular)`
   width: 4.5em;
 `
 
+const TopRightCorner = styled.div`
+  position: fixed;
+  top: 2rem;
+  right: 2rem;
+  z-index: 1000;
+  display: flex;
+`
+
+const CornerRabbitLink = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  text-decoration: none;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const PixelRabbitIcon = styled(GatsbyImage)`
+  width: 3rem !important;
+  height: 3rem !important;
+  border-radius: 0.3rem;
+  filter: drop-shadow(0 2px 8px rgba(0, 0, 0, 0.4));
+  image-rendering: -webkit-optimize-contrast;
+  image-rendering: crisp-edges;
+  image-rendering: pixelated;
+`
+
+const MonogramText = styled.p`
+  font-family: 'Monogram', monospace;
+  font-size: 0.8rem;
+  color: white;
+  margin: 0;
+  text-shadow: 0 1px 3px rgba(0, 0, 0, 0.7);
+  text-align: center;
+  white-space: nowrap;
+  cursor: pointer;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
+
 export default function Home() {
+  const data = useStaticQuery(graphql`
+    query {
+      pixelRabbit: file(
+        sourceInstanceName: { eq: "images" }
+        name: { eq: "pixel_rabbit" }
+      ) {
+        childImageSharp {
+          gatsbyImageData(width: 80, height: 80, layout: FIXED, quality: 100)
+        }
+      }
+    }
+  `)
+
+  const pixelRabbitImage = getImage(data.pixelRabbit)
+
   return (
     <MainContainer>
+      <MonogramFont />
       <GlobalStyles gradient whitetext noscroll />
       <HomeHeader>Andria Hibe</HomeHeader>
       <Divider />
@@ -101,6 +174,19 @@ export default function Home() {
           <StyledLinkedinLinkIcon />
         </a>
       </IconContainer>
+      <TopRightCorner>
+        <CornerRabbitLink
+          href="https://andria-hibe.github.io/2d-portfolio-cottage/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <MonogramText>Check out my interactive portfolio!</MonogramText>
+          <PixelRabbitIcon
+            image={pixelRabbitImage}
+            alt="Interactive Portfolio"
+          />
+        </CornerRabbitLink>
+      </TopRightCorner>
       <CardContainer>
         <HomeCard>
           <GatsbyLink to="/resume">Resume</GatsbyLink>
